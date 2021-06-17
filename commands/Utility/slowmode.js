c4d52b48024b87b8ec0b798f9419af44ed4893b9
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-06-18 09:00:01 +1000

    🐛🎨 Fix slowmode bot output being inconsistant

--- commands/Utility/slowmode.js
+++ commands/Utility/slowmode.js
@@ -1,53 +1,35 @@
 const Discord = require('discord.js-light');
 require('discord-reply');
-const config = require('../../config.json');
 const footer = ('Bot by xMdb#7897')
 
 module.exports = {
     name: 'slowmode',
-    aliases: ['sm', 'setslowmode'],
+    aliases: ['sm', 'setslowmode', 'delay', 'slow'],
     description: 'Allows a custom slowmode value to be set',
     usage: '[seconds]',
     cooldown: 5,
     execute(message, args) {
-        if (!args.length || isNaN(args)) {
-            const noArgs = new Discord.MessageEmbed()
-                .setColor('RED')
-                .setDescription(`Please include a value in **seconds** that is below **6 hours**.`)
-                .setTimestamp()
-                .setFooter(footer);
-            return message.lineReply(noArgs);
-        }
-        if (args > 21600) {
-            const largeArgs = new Discord.MessageEmbed()
-                .setColor('RED')
-                .setDescription(`Slowmode length must be under **6 hours**.`)
-                .setTimestamp()
-                .setFooter(footer);
-            return message.lineReply(largeArgs);
-        }
-        const slowmodeSec = args.join(' ');
-        const slowmodeFailure = new Discord.MessageEmbed()
+        const noPerms = new Discord.MessageEmbed()
             .setColor('RED')
             .setDescription(`You don't have permission to use this command.`)
             .setTimestamp()
             .setFooter(footer);
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) {
-            message.lineReply(slowmodeFailure);
-        } else {
-            const slowmodeSuccess = new Discord.MessageEmbed()
-                .setColor('#3A783F')
-                .setDescription(`Success! The slowmode in this channel is now set to **${slowmodeSec}** seconds!`)
-                .setTimestamp()
-                .setFooter(footer);
-            message.lineReply(slowmodeSuccess);
-            message.channel.setRateLimitPerUser(slowmodeSec, `Executed by ${message.author.username}#${message.author.discriminator}`).catch(error => {
-                if (error.code === 50013) {
-                    console.error(error);
-                    message.channel.send(`${message.author}, I do not have the correct permissions to perform that task.`);
-                    return;
-                }
+        const invalidArgs = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setDescription(`Please include a value in **seconds** that is below **6 hours**.`)
+            .setTimestamp()
+            .setFooter(footer);
+        const success = new Discord.MessageEmbed()
+            .setColor('#3A783F')
+            .setDescription(`Success! The slowmode in this channel is now set to **${args}** seconds!`)
+            .setTimestamp()
+            .setFooter(footer);
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.lineReply(noPerms);
+        if (!args.length || isNaN(args) || args > 21600) return message.lineReply(invalidArgs);
+        message.channel.setRateLimitPerUser(args, `Executed by ${message.author.username}#${message.author.discriminator}`)
+            .then(message.lineReply(success))
+            .catch(error => {
+                if (error.code === 50013) return message.channel.send(`${message.author}, I do not have the correct permissions to perform that task.`);
             });
-        }
     }
 };
