e3bdf5ba58ada3b264cb9c7d61206e73dd870058
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-06-18 11:23:26 +1000

    🐛 Actually fix slowmode error handling

--- commands/Utility/slowmode.js
+++ commands/Utility/slowmode.js
@@ -8,7 +8,8 @@ module.exports = {
     description: 'Allows a custom slowmode value to be set',
     usage: '[seconds]',
     cooldown: 5,
-    execute(message, args) {
+    async execute(message, args) {
+        const value = args.join(' ');
         const noPerms = new Discord.MessageEmbed()
             .setColor('RED')
             .setDescription(`You don't have permission to use this command.`)
@@ -21,15 +22,19 @@ module.exports = {
             .setFooter(footer);
         const success = new Discord.MessageEmbed()
             .setColor('#3A783F')
-            .setDescription(`Success! The slowmode in this channel is now set to **${args}** seconds!`)
+            .setDescription(`Success! The slowmode in this channel is now set to **${value}** seconds!`)
             .setTimestamp()
             .setFooter(footer);
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.lineReply(noPerms);
-        if (!args.length || isNaN(args) || args > 21600) return message.lineReply(invalidArgs);
-        message.channel.setRateLimitPerUser(args, `Executed by ${message.author.username}#${message.author.discriminator}`)
-            .then(message.lineReply(success))
-            .catch(error => {
-                if (error.code === 50013) return message.channel.send(`${message.author}, I do not have the correct permissions to perform that task.`);
-            });
+        if (!value.length || isNaN(value) || value > 21600) return message.lineReply(invalidArgs);
+        try {
+            await message.channel.setRateLimitPerUser(value, `Executed by ${message.author.username}#${message.author.discriminator}`)
+        } catch (error) {
+            if (error.code === 50013) {
+                message.channel.send(`${message.author}, I do not have the correct permissions to perform that task.`);
+                return;
+            }
+        }
+        message.lineReply(success);
     }
 };
