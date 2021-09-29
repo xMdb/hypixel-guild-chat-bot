47a52df3ead6d63c6b311f67cad70b084561b94a
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-09-29 22:58:56 +1000

    🐛 Remove md from inbound chat

--- events/minecraft/chat/guildChat.js
+++ events/minecraft/chat/guildChat.js
@@ -1,9 +1,10 @@
+const { Util } = require('discord.js');
 const { minebot, toDiscordChat } = require('../../../app');
 
 module.exports = {
    name: 'guildChat',
    async execute(rank, playername, grank, message) {
       if (playername === minebot.username) return;
-      toDiscordChat(`<a:MC:829592987616804867> **${rank ?? ''}${playername}: ${message}**`);
+      toDiscordChat(`<a:MC:829592987616804867> **${rank ?? ''}${playername}: ${Util.escapeMarkdown(message)}**`);
    },
 };
