51f0c859fb0bd5d824baeab8908c5b4b2a7d0f3a
Matt <46137770+xMdb@users.noreply.github.com>
2022-01-26 10:43:37 +1000

    fix(commands): make register command actually register

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -18,5 +18,51 @@ module.exports = {
       await bot.guilds.cache.get(config.ids.server).channels.cache.get(config.ids.guildChannel).send({
          content: `<:yes:829640052531134464> Bot has reconnected to Discord.`,
       });
+
+      // register register command
+
+      const data = {
+         name: 'register',
+         description: 'Register command',
+         defaultPermission: false,
+         options: [
+            {
+               name: 'command',
+               type: 'STRING',
+               description: 'The command to register',
+               required: true,
+            },
+            {
+               name: 'type',
+               type: 'STRING',
+               description: 'Register the command in this guild or globally',
+               required: true,
+               choices: [
+                  {
+                     name: 'As a guild command',
+                     value: 'guild',
+                  },
+                  {
+                     name: 'As a global command',
+                     value: 'global',
+                  },
+               ],
+            },
+         ],
+      };
+
+      // —— Set command permissions
+      const permissions = [
+         {
+            id: config.ids.owner,
+            type: 'USER',
+            permission: true,
+         },
+      ];
+
+      const commandProd = await bot.guilds.cache.get(config.ids.server)?.commands.create(data);
+      const commandDev = await bot.guilds.cache.get(config.ids.testingServer)?.commands.create(data);
+      await commandProd.permissions.add({ permissions });
+      await commandDev.permissions.add({ permissions });
    },
 };
